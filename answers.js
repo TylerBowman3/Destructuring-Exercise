1d382df19f68db8d14264879b55d7fd60c56e137
Tyler Bowman <114122616+TylerBowman3@users.noreply.github.com>
2024-01-16 11:28:27 -0500

    submitting answers

---
+++ answers.js
@@ -0,0 +1,12 @@
+// 1 
+
+const {a, b} = obj.numbers
+
+// 2
+
+[arr[0], arr[1]] = [arr[1], arr[0]]
+
+// 3
+
+const raceResults() => ([first, second, third, ...rest]) => ({first, second, third, rest})
+
